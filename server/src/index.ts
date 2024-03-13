c2144118d5080a87775713c5cf5ffa4a9b06c426
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-03-13 17:27:01 +0800

    Update PORT and app variable declarations

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,8 @@ import records from './routes/record';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-const app = express();
+const PORT: number = Number(process.env.PORT) || 5000;
+const app: express.Application = express();
 
 app.use(cors());
 app.use(express.json());
