726e5ecc74301c92ab29b134eb1693d573512b47
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-03-13 17:55:14 +0800

    Add CORS options to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,12 @@ dotenv.config();
 const PORT: number = Number(process.env.PORT) || 5000;
 const app: express.Application = express();
 
-app.use(cors());
+const corsOptions: cors.CorsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/record', records);
 
